3dfcb3c5ab7904bcfea0b84d1ec30531d7c7084d
ray247k <ray247k@gmail.com>
2016-12-11 20:33:52 +0800

    使用@方法讓使用者輸入空白帳號密碼時不回報錯誤信息

--- PHP275/cookie/LiginCookie/login.php
+++ PHP275/cookie/LiginCookie/login.php
@@ -17,8 +17,8 @@
             setcookie('id',$_POST['id'],date('U')+40);
             setcookie('pwd',$_POST['pwd'],date('U')+40);
             echo "歡迎登入：".$_POST['id']."<br>"."這是主頁內容";
-        }elseif(($_COOKIE['id']==$db_id) && ($_COOKIE['pwd']==$db_pwd)){
-            echo $_COOKIE['id']." 歡迎回來<br>這是主頁內容";
+        }elseif((@$_COOKIE['id']==$db_id) && (@$_COOKIE['pwd']==$db_pwd)){
+            echo @$_COOKIE['id']." 歡迎回來<br>這是主頁內容";
         }else{
             echo "您尚未登入<br><a href='input.html'>回上頁</a>";
         }
